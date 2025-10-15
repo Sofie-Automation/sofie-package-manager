0a9f849d4b508099245d4f70550ca3b43f602e1b
Julian Waller <git@julusian.co.uk>
2025-10-15 15:29:18 +0100

    wip

--- apps/worker/app/docker-entrypoint.sh
+++ apps/worker/app/docker-entrypoint.sh
@@ -3,4 +3,5 @@ set -e
 
 HOSTNAME=$(hostname)
 
+# Inject a unique worker ID based on the hostname, and disable the appContainer connection
 node dist/index.js --workerId=$HOSTNAME --appContainerURL="" $*
