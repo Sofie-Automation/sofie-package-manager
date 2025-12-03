6490649d2a1b2976f528d0cba52ed1cdaf2e9af3
Johan Nyman <johan@nytamin.se>
2025-12-03 07:57:18 +0100

    chore: improve build script

--- scripts/cleanup-after-build32.mjs
+++ scripts/cleanup-after-build32.mjs
@@ -8,24 +8,53 @@ const basePath = '.'
 
 console.log(`Cleaning up...`)
 
-await fse.rm(path.resolve(path.join(basePath, 'tmp_packages_for_build')), {
-	recursive: true,
-})
-
-// Remove things that arent used, to reduce file size:
-const ps = []
-console.log(`Remove unused files...`)
-const copiedFiles = [
-	...(await glob(`${basePath}/apps/*/app/node_modules/@*/app/*`)),
-	...(await glob(`${basePath}/apps/*/app/node_modules/@*/generic/*`)),
-	...(await glob(`${basePath}/node_modules/@parcel/watcher/build/**/*`)),
-]
-
-console.log('copiedFiles', copiedFiles)
-for (const file of copiedFiles) {
-	console.log(`Removing file: "${file}"`)
-	ps.push(fse.rm(file, { recursive: true }))
+try {
+	await fse.rm(path.resolve(path.join(basePath, 'tmp_packages_for_build')), {
+		recursive: true,
+	})
+} catch (e) {
+	if (`${e}`.includes('no such file or directory')) {
+		// Ignore
+	} else {
+		throw e
+	}
 }
-await Promise.all(ps)
+
+// Remove artifacts created by prepare-for-build32.js...
+
+console.log(`Looking up copied files to remove...`)
+
+const filesToRemove = []
+
+async function lookupFiles(pattern) {
+	console.log(`Checking ${pattern}...`)
+
+	const files = await glob(`${basePath}/${pattern}`)
+	for (const file of files) {
+		filesToRemove.push(file)
+	}
+}
+
+await lookupFiles(`apps/*/app/node_modules/@*/app/*`)
+await lookupFiles(`apps/*/app/node_modules/@*/generic/*`)
+await lookupFiles(`node_modules/@parcel/watcher/build/**/*`)
+
+console.log(`Found ${filesToRemove.length} files to remove.`)
+
+const activeRemovals = []
+
+while (filesToRemove.length > 0) {
+	// Remove up to 10 files at a time:
+	for (let i = 0; i < 10; i++) {
+		const fileToRemove = filesToRemove.shift()
+		if (!fileToRemove) break
+
+		console.log(`Removing file: "${fileToRemove}"`)
+		activeRemovals.push(fse.rm(fileToRemove, { recursive: true }))
+	}
+
+	await Promise.all(activeRemovals)
+	activeRemovals.length = 0
+}
 
 console.log(`...done!`)
