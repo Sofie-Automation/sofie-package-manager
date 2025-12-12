fe9181b122b2e3670c917b922f87005e8bad685f
Robert (Jamie) Munro <rjmunro@arjam.net>
2025-12-12 01:02:58 +0000

    Fix mocks for node: prefix

--- apps/appcontainer-node/packages/generic/jest.config.js
+++ apps/appcontainer-node/packages/generic/jest.config.js
@@ -4,4 +4,7 @@ const packageJson = require('./package')
 module.exports = {
 	...base,
 	displayName: packageJson.name,
+	moduleNameMapper: {
+		'^node:child_process$': '<rootDir>/src/__mocks__/child_process.ts',
+	},
 }
--- tests/internal-tests/jest.config.js
+++ tests/internal-tests/jest.config.js
@@ -4,4 +4,8 @@ const packageJson = require('./package')
 module.exports = {
 	...base,
 	displayName: packageJson.name,
+	moduleNameMapper: {
+		'^node:fs$': '<rootDir>/src/__mocks__/fs.ts',
+		'^node:child_process$': '<rootDir>/src/__mocks__/child_process.ts',
+	},
 }
