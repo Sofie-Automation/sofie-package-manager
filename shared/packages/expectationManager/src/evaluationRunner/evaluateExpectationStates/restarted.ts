83f0c2df0bb249fba6923bff8228df98992654a1
Johan Nyman <johan@nytamin.se>
2026-03-02 08:29:44 +0100

    fix: clear state when RESTARTING expectations

--- shared/packages/expectationManager/src/evaluationRunner/evaluateExpectationStates/restarted.ts
+++ shared/packages/expectationManager/src/evaluationRunner/evaluateExpectationStates/restarted.ts
@@ -13,6 +13,11 @@ export async function evaluateExpectationStateRestarted({
 }: EvaluateContext): Promise<void> {
 	assertState(trackedExp, ExpectedPackageStatusAPI.WorkStatusState.RESTARTED)
 
+	// Clear this state, so that they will be repopulated in NEW:
+	trackedExp.queriedWorkers.clear()
+	trackedExp.availableWorkers.clear()
+	trackedExp.noAvailableWorkersReasons.clear()
+
 	await manager.workerAgents.assignWorkerToSession(trackedExp)
 	if (trackedExp.session.assignedWorker) {
 		// Start by removing the expectation
