6dcf95e4b3f14e884e02445305313b889e49bab6
Julian Waller <git@julusian.co.uk>
2026-03-17 12:50:46 +0000

    chore: update koa router

--- apps/http-server/packages/generic/src/lib.ts
+++ apps/http-server/packages/generic/src/lib.ts
@@ -1,9 +1,8 @@
-import Koa from 'koa'
-import Router from 'koa-router'
+import type { RouterContext } from '@koa/router'
 import { Readable, Writable } from 'stream'
 
-export type CTX = Koa.ParameterizedContext<any, Router.IRouterParamContext<any, any>>
-export type CTXPost = Koa.ParameterizedContext<any, Router.IRouterParamContext<any, any>>
+export type CTX = RouterContext<any, any>
+export type CTXPost = RouterContext<any, any>
 
 export async function asyncPipe(readable: Readable, writable: Writable): Promise<void> {
 	return new Promise((resolve) => {
